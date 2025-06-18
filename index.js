c396d85acae331d9a9a365e1eba1112d3d1201e1
franko122 <83500527+franko122@users.noreply.github.com>
2025-06-18 05:13:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ three.addEventListener('click' , ()=>{
     let ede = document.getElementById("ede")
     let three = document.getElementById("three"); 
         let Username = "keiko" 
-        let password = "lukas&keiko"   
+        let password = "chinedu"   
         if  ( one.value == Username && two.value == password) {  
             wel.style.display="none"
             dasher.style.display='block'
